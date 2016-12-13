4c442028da4647e5cc46803cf02324939bbd6aed
Chan11 <pchandrahas10@gmail.com>
2016-12-13 23:24:07 +0530

    change port

--- app/index.js
+++ app/index.js
@@ -25,7 +25,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 _db2.default.connectMongo();
 
 var app = (0, _express2.default)();
-app.set('port', 9000);
+app.set('port', process.env.PORT);
 app.use(_express2.default.static(_path2.default.join(__dirname, './build')));
 app.use(_bodyParser2.default.json());
 
